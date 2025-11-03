87d4c665b229f6ee7da7de024b4cb49e1c74f111
tandw1r0 <denisaikros@gmail.com>
2025-11-03 17:58:15 +0300

    Update Лаба6.py

--- Лаба6.py
+++ Лаба6.py
@@ -1,7 +1,6 @@
-# Задачи "Практика 6"
-# Задача 6.3
-# 3.1)
-print("Задача 6 - 3.1")
+# Практика 6
+
+print("Номер 3.1")
 def number1():
     D = [5, 10, 15, 8, 16, 3, 6, 9, 4, 11]
     n = len(D)
@@ -9,31 +8,27 @@ def number1():
     for i in range(n):
         if i % 2 != 0:
             k1 += D[i]
-
     print(D)
     print(k1)
-
 number1()
+print()
 
-# 3.2)
-print("Задача 6 - 3.2")
+print("Номер 3.2")
 def number2():
     m = [10, 20, 5, 30, 12, 25, 8, 18]
-    print("Исходное массив: ", m)
+    print(m)
 
     for i in range(len(m)):
         if m[i] < 15:
             m[i] *= 2
 
-    print("Новый массив: ", m)
-
+    print(m)
 number2()
+print()
 
 
-# 8.1)
-print("Задача 8.1")
+print("Номер 8.1")
 s = [12, 10, 5, 8, 2, 11, 16, 9]
-print("Исходный массив: ", s)
 summ = sum(s)
 
 k = 1
@@ -44,7 +39,6 @@ print("Сумма: ", summ)
 print("Производная: ", k)
 print()
 
-# 8.2
 print("Задача 8.2")
 num = [1.8, 0, 2.9, 3.1, 0, 2.5, 5.6, 0]
 srzanch = sum(num) / len(num)
