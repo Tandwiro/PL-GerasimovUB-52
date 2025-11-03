130c8078307d303a7b141596a23cdca99da9dad0
tandw1r0 <denisaikros@gmail.com>
2025-11-03 11:16:19 +0300

    Delete Лаба1.py

--- Лаба1.py
+++
@@ -1,9 +0,0 @@
-# Задачи "Практика 1"
-# Задача 1.1
-
-name = input("Ваши фамилия, имя? ")
-age = input("Сколько Вам лет? ")
-place_of_residence = input("Где вы живете? ")
-print("- ФИО:", name)
-print("- Возраст:", age)
-print("- Место жительства:", place_of_residence)
