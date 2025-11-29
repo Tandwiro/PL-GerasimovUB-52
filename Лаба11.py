9a880f15d79c59dc76858d4395560532b1024f97
tandw1r0 <denisaikros@gmail.com>
2025-11-29 17:25:13 +0300

    Update Лаба11.py

--- Лаба11.py
+++ Лаба11.py
@@ -1,45 +1,54 @@
 # Практика 11
+# Создание GUI
 from tkinter import *
+from tkinter import ttk
 from tkinter import messagebox, filedialog
 
 window = Tk()
 window.title("Герасимов Денис Павлович")
-window.geometry("800x480")
+window.geometry("420x420")
+
+
+notebook = ttk.Notebook(window)
+notebook.pack(fill=BOTH, expand=1)
+
+tab1 = ttk.Frame(notebook)
+notebook.add(tab1, text='Калькулятор')
+
+tab2 = ttk.Frame(notebook)
+notebook.add(tab2, text='Чекбоксы')
+
+tab3 = ttk.Frame(notebook)
+notebook.add(tab3, text='Текст из файла')
 
 
 # Задание 1
-num1 = Label(window,
-            text="Задание 1",
-            font=("Arial Bold", 11),
-            width=20, height=2
-            )
-num1.grid(column=0, row=0)
-lbl1 = Label(window,
+lbl1 = Label(tab1,
             text="Калькулятор",
             font=("Arial Bold", 14),
-            width=20, height=2
+            width=20, height=3
             )
-lbl1.grid(column=0, row=1)
+lbl1.grid(column=20, row=0)
 
-txt1 = Entry(window, width=10)
-txt1.grid(column=0, row=2)
+txt1 = Entry(tab1, width=10)
+txt1.grid(column=20, row=1)
 
-txt2 = Entry(window, width=10)
-txt2.grid(column=0, row=3)
+txt2 = Entry(tab1, width=10)
+txt2.grid(column=20, row=2)
 
 opvar = StringVar()
 opvar.set("+")
 
 oper = ["+", "-", "*", "/"]
-opmenu = OptionMenu(window, opvar, *oper)
-opmenu.grid(column=0, row=4)
+opmenu = OptionMenu(tab1, opvar, *oper)
+opmenu.grid(column=20, row=3)
 
-oplbl = Label(window,
+oplbl = Label(tab1,
               text="Вывод:",
               font=("Arial Bold", 12),
-              width=20, height=2
+              width=45, height=3
               )
-oplbl.grid(column=0, row=5)
+oplbl.grid(column=20, row=4)
 
 
 def calculator():
@@ -57,49 +66,44 @@ def calculator():
 
     oplbl.config(text=f"Вывод: {res}")
 
-calc = Button(window,
+
+calc = Button(tab1,
               text="ОТВЕТ",
               command = calculator
               )
-calc.grid(column=0, row=6)
+calc.grid(column=20, row=5)
 
 
 # Задание 2
-num2 = Label(window,
-             text="Задание 2",
-             font=("Arial Bold", 11),
-             width=20, height=2
-             )
-num2.grid(column=3, row=0)
-
-lbl2 = Label(window,
+lbl2 = Label(tab2,
              text="Чекбоксы",
              font=("Arial Bold", 14),
-             width=20, height=2)
-lbl2.grid(column=3, row=1)
+             width=35, height=2)
+lbl2.grid(column=3, row=0)
 
 
 chk_var1 = BooleanVar()
 chk_var2 = BooleanVar()
 chk_var3 = BooleanVar()
 
-chk1 = Checkbutton(window,
+
+chk1 = Checkbutton(tab2,
                   text="Первый",
                   variable=chk_var1
                   )
-chk1.grid(column=3, row=2)
+chk1.grid(column=3, row=1)
 
-chk2 = Checkbutton(window,
+chk2 = Checkbutton(tab2,
                    text="Второй",
                    variable=chk_var2
                    )
-chk2.grid(column=3, row=3)
+chk2.grid(column=3, row=2)
 
-chk3 = Checkbutton(window,
+chk3 = Checkbutton(tab2,
                    text="Третий",
                    variable=chk_var3
                    )
-chk3.grid(column=3, row=4)
+chk3.grid(column=3, row=3)
 
 def ckbox():
     select = []
@@ -115,31 +119,24 @@ def ckbox():
     else:
         messagebox.showinfo("Выбор", "Ничего")
 
-chk_btn = Button(window,
+chk_btn = Button(tab2,
                 text="Показать",
                 command=ckbox
                 )
-chk_btn.grid(column=3, row=5)
+chk_btn.grid(column=3, row=4)
 
 
 # Задание 3
-num3 = Label(window,
-             text="Задание 3",
-             font=("Arial Bold", 11),
-             width=20, height=2
-             )
-num3.grid(column=6, row=0)
-
-lbl3 = Label(window,
+lbl3 = Label(tab3,
              text="Текст",
              font=("Arial Bold", 14),
-             width=20, height=2
+             width=35, height=2
              )
-lbl3.grid(column=6, row=1)
+lbl3.grid(column=6, row=0)
 
-text3 = Text(window,
-             width=30, height=1)
-text3.grid(column=6, row=2)
+text3 = Text(tab3,
+             width=30, height=10)
+text3.grid(column=6, row=1)
 
 
 def menu():
@@ -156,13 +153,10 @@ def menu():
         messagebox.showinfo("Ошибка", "Не вышло")
 
 
-byn_load = Button(window,
+byn_load = Button(tab3,
                   text="Загрузить файл",
                   command=menu
                   )
-byn_load.grid(column=6, row=3)
-
+byn_load.grid(column=6, row=2)
 
 window.mainloop()
-
-
